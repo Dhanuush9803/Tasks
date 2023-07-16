4031030aa89f60ff6ab50ac42b5bbe690b63c547
Dhanuush9803 <134191804+Dhanuush9803@users.noreply.github.com>
2023-07-16 09:23:39 +0530

    Add files via upload

---
+++ 1_SQL_table_creation.sql
@@ -0,0 +1,153 @@
+
+
+
+CREATE TABLE CUSTOMER(CUST_ID TINYINT, CUST_NAME VARCHAR(30),AGE TINYINT, MAIL_ID VARCHAR(100), PHONE_N0 BIGINT,ADDRESS VARCHAR(50), CUST_DATE DATE, IN_TIME TIMESTAMP, OUT_TIME TIMESTAMP, SOCIAL_MEDIA VARCHAR(50), PRIMARY KEY(CUST_ID));
+
+
+INSERT INTO CUSTOMER VALUES(17, 'Manish', 28, 'Manish@gmail.com', '9876543210', '123 Street, City', '2023-06-15', '2023-06-15 09:00:00', '2023-06-15 10:30:00', 'twitter'),
+    (18, 'Manish', 28, 'Manish@gmail.com', '9876543210', '123 Street, City', '2023-06-15', '2023-06-15 11:30:00', '2023-06-15 13:00:00', 'facebook'),
+    (19, 'Manish', 28, 'Manish@gmail.com', '9876543210', '123 Street, City', '2023-06-15', '2023-06-15 14:30:00', '2023-06-15 16:00:00', 'instagram'),
+    (20, 'Manish', 28, 'Manish@gmail.com', '9876543210', '123 Street, City', '2023-06-15', '2023-06-15 17:00:00', '2023-06-15 18:30:00', 'linkedin'),
+    (21, 'Manish', 28, 'Manish@gmail.com', '9876543210', '123 Street, City', '2023-06-15', '2023-06-15 19:00:00', '2023-06-15 20:30:00', 'youtube');
+    
+INSERT INTO CUSTOMER VALUES
+    (22, 'Abirami', 35, 'Abirami@gmail.com', '9876543210', '56 Avenue, Town', '2023-06-15', '2023-06-15 09:00:00', '2023-06-15 10:30:00', 'twitter'),
+    (23, 'Abirami', 35, 'Abirami@gmail.com', '9876543210', '56 Avenue, Town', '2023-06-15', '2023-06-15 11:30:00', '2023-06-15 13:00:00', 'facebook'),
+    (24, 'Abirami', 35, 'Abirami@gmail.com', '9876543210', '56 Avenue, Town', '2023-06-15', '2023-06-15 14:30:00', '2023-06-15 16:00:00', 'instagram'),
+    (25, 'Abirami', 35, 'Abirami@gmail.com', '9876543210', '56 Avenue, Town', '2023-06-15', '2023-06-15 17:00:00', '2023-06-15 18:30:00', 'linkedin'),
+    (26, 'Abirami', 35, 'Abirami@gmail.com', '9876543210', '56 Avenue, Town', '2023-06-15', '2023-06-15 19:00:00', '2023-06-15 20:30:00', 'youtube');
+
+INSERT INTO CUSTOMER VALUES
+(27, 'John', 30, 'John@gmail.com', '9876543210', '789 Road, Village', '2023-06-15', '2023-06-15 09:00:00', '2023-06-15 10:30:00', 'twitter'),
+    (28, 'John', 30, 'John@gmail.com', '9876543210', '789 Road, Village', '2023-06-15', '2023-06-15 11:30:00', '2023-06-15 13:00:00', 'facebook'),
+    (29, 'John', 30, 'John@gmail.com', '9876543210', '789 Road, Village', '2023-06-15', '2023-06-15 14:30:00', '2023-06-15 16:00:00', 'instagram'),
+    (30, 'John', 30, 'John@gmail.com', '9876543210', '789 Road, Village', '2023-06-15', '2023-06-15 17:00:00', '2023-06-15 18:30:00', 'linkedin'),
+    (31, 'John', 30, 'John@gmail.com', '9876543210', '789 Road, Village', '2023-06-15', '2023-06-15 19:00:00', '2023-06-15 20:30:00', 'youtube');
+    
+INSERT INTO CUSTOMER VALUES
+(32, 'Dany', 40, 'Dany@gmail.com', '9876543210', '456 Oak St', '2023-06-15', '2023-06-15 09:00:00', '2023-06-15 10:30:00', 'twitter'),
+    (33, 'Dany', 40, 'Dany@gmail.com', '9876543210', '456 Oak St', '2023-06-15', '2023-06-15 11:30:00', '2023-06-15 13:00:00', 'facebook'),
+    (34, 'Dany', 40, 'Dany@gmail.com', '9876543210', '456 Oak St', '2023-06-15', '2023-06-15 14:30:00', '2023-06-15 16:00:00', 'instagram'),
+    (35, 'Dany', 40, 'Dany@gmail.com', '9876543210', '456 Oak St', '2023-06-15', '2023-06-15 17:00:00', '2023-06-15 18:30:00', 'linkedin'),
+    (36, 'Dany', 40, 'Dany@gmail.com', '9876543210', '456 Oak St', '2023-06-15', '2023-06-15 19:00:00', '2023-06-15 20:30:00', 'youtube');
+    
+  INSERT INTO CUSTOMER VALUES  
+    (37, 'Jessica', 27, 'jessica@gmail.com', '9876543210', '789 Road, Village', '2023-06-15', '2023-06-15 09:00:00', '2023-06-15 10:30:00', 'twitter'),
+    (38, 'Jessica', 27, 'jessica@gmail.com', '9876543210', '789 Road, Village', '2023-06-15', '2023-06-15 11:30:00', '2023-06-15 13:00:00', 'facebook'),
+    (39, 'Jessica', 27, 'jessica@gmail.com', '9876543210', '789 Road, Village', '2023-06-15', '2023-06-15 14:30:00', '2023-06-15 16:00:00', 'instagram'),
+    (40, 'Jessica', 27, 'jessica@gmail.com', '9876543210', '789 Road, Village', '2023-06-15', '2023-06-15 17:00:00', '2023-06-15 18:30:00', 'linkedin'),
+    (41, 'Jessica', 27, 'jessica@gmail.com', '9876543210', '789 Road, Village', '2023-06-15', '2023-06-15 19:00:00', '2023-06-15 20:30:00', 'youtube');
+    
+INSERT INTO CUSTOMER VALUES
+(42, 'Jack', 33, 'john@gmail.com', '9876543210', '321 Lane, County', '2023-06-15', '2023-06-15 09:00:00', '2023-06-15 10:30:00', 'twitter'),
+    (43, 'Jack', 33, 'john@gmail.com', '9876543210', '321 Lane, County', '2023-06-15', '2023-06-15 11:30:00', '2023-06-15 13:00:00', 'facebook'),
+    (44, 'Jack', 33, 'john@gmail.com', '9876543210', '321 Lane, County', '2023-06-15', '2023-06-15 14:30:00', '2023-06-15 16:00:00', 'instagram'),
+    (45, 'Jack', 33, 'john@gmail.com', '9876543210', '321 Lane, County', '2023-06-15', '2023-06-15 17:00:00', '2023-06-15 18:30:00', 'linkedin'),
+    (46, 'Jack', 33, 'john@gmail.com', '9876543210', '321 Lane, County', '2023-06-15', '2023-06-15 19:00:00', '2023-06-15 20:30:00', 'youtube');
+    
+INSERT INTO CUSTOMER VALUES
+(67, 'Subu', 32, 'Subu@gmail.com', '9876543210', '123 Elm St', '2023-06-15', '2023-06-15 09:00:00', '2023-06-15 10:30:00', 'twitter'),
+    (68, 'Subu', 32, 'Subu@gmail.com', '9876543210', '123 Elm St', '2023-06-15', '2023-06-15 11:30:00', '2023-06-15 13:00:00', 'facebook'),
+    (69, 'Subu', 32, 'Subu@gmail.com', '9876543210', '123 Elm St', '2023-06-15', '2023-06-15 14:30:00', '2023-06-15 16:00:00', 'instagram'),
+    (70, 'Subu', 32, 'Subu@gmail.com', '9876543210', '123 Elm St', '2023-06-15', '2023-06-15 17:00:00', '2023-06-15 18:30:00', 'linkedin'),
+    (71, 'Subu', 32, 'Subu@gmail.com', '9876543210', '123 Elm St', '2023-06-15', '2023-06-15 19:00:00', '2023-06-15 20:30:00', 'youtube');
+
+INSERT INTO CUSTOMER VALUES
+(72, 'Ram', 27, 'Ram@gmail.com', '9876543210', '456 Maple St', '2023-06-15', '2023-06-15 09:00:00', '2023-06-15 10:30:00', 'twitter'),
+    (73, 'Ram', 27, 'Ram@gmail.com', '9876543210', '456 Maple St', '2023-06-15', '2023-06-15 11:30:00', '2023-06-15 13:00:00', 'facebook'),
+    (74, 'Ram', 27, 'Ram@gmail.com', '9876543210', '456 Maple St', '2023-06-15', '2023-06-15 14:30:00', '2023-06-15 16:00:00', 'instagram'),
+    (75, 'Ram', 27, 'Ram@gmail.com', '9876543210', '456 Maple St', '2023-06-15', '2023-06-15 17:00:00', '2023-06-15 18:30:00', 'linkedin'),
+    (76, 'Ram', 27, 'Ram@gmail.com', '9876543210', '456 Maple St', '2023-06-15', '2023-06-15 19:00:00', '2023-06-15 20:30:00', 'youtube');
+    
+INSERT INTO CUSTOMER VALUES
+(82, 'Prasad', 35, 'Prasad@gmail.com', '9876543210', '987 Highway, State', '2023-06-15', '2023-06-15 09:00:00', '2023-06-15 10:30:00', 'twitter'),
+    (83, 'Prasad', 35, 'Prasad@gmail.com', '9876543210', '987 Highway, State', '2023-06-15', '2023-06-15 11:30:00', '2023-06-15 13:00:00', 'facebook'),
+    (84, 'Prasad', 35, 'Prasad@gmail.com', '9876543210', '987 Highway, State', '2023-06-15', '2023-06-15 14:30:00', '2023-06-15 16:00:00', 'instagram'),
+    (85, 'Prasad', 35, 'Prasad@gmail.com', '9876543210', '987 Highway, State', '2023-06-15', '2023-06-15 17:00:00', '2023-06-15 18:30:00', 'linkedin'),
+    (86, 'Prasad', 35, 'Prasad@gmail.com', '9876543210', '987 Highway, State', '2023-06-15', '2023-06-15 19:00:00', '2023-06-15 20:30:00', 'youtube');
+
+INSERT INTO CUSTOMER VALUES
+(87, 'Kavin', 31, 'Kavin@gmail.com', '9876543210', '456 Oak St', '2023-06-15', '2023-06-15 09:00:00', '2023-06-15 10:30:00', 'twitter'),
+    (88, 'Kavin', 31, 'Kavin@gmail.com', '9876543210', '456 Oak St', '2023-06-15', '2023-06-15 11:30:00', '2023-06-15 13:00:00', 'facebook'),
+    (89, 'Kavin', 31, 'Kavin@gmail.com', '9876543210', '456 Oak St', '2023-06-15', '2023-06-15 14:30:00', '2023-06-15 16:00:00', 'instagram'),
+    (90, 'Kavin', 31, 'Kavin@gmail.com', '9876543210', '456 Oak St', '2023-06-15', '2023-06-15 17:00:00', '2023-06-15 18:30:00', 'linkedin'),
+    (91, 'Kavin', 31, 'Kavin@gmail.com', '9876543210', '456 Oak St', '2023-06-15', '2023-06-15 19:00:00', '2023-06-15 20:30:00', 'youtube');
+
+SELECT * FROM CUSTOMER;
+
+
+CREATE TABLE ORDER_TABLE (orderID int primary key,Customer_ID tinyint,foreign key(Customer_ID)references CUSTOMER(CUST_ID),date_purchased date,product_ID varchar(10),product_name varchar(50),arrival_date date,dispatch_date date,sender_location varchar(20),receiver_location varchar(20));
+
+
+INSERT INTO ORDER_TABLE VALUES (11, 17, '2023-06-20', 'p011', 'electronics', '2023-06-21', '2023-06-25', 'Location_F', 'Location_B'),
+    (12, 18, '2023-05-12', 'p012', 'books', '2023-05-15', '2023-05-18', 'Location_A', 'Location_C'),
+    (13, 19, '2023-04-05', 'p013', 'toys', '2023-04-06', '2023-04-10', 'Location_E', 'Location_H'),
+    (14, 20, '2023-03-10', 'p014', 'clothing', '2023-03-12', '2023-03-15', 'Location_B', 'Location_G'),
+    (15, 21, '2023-02-15', 'p015', 'home decor', '2023-02-18', '2023-02-20', 'Location_D', 'Location_I');
+    
+INSERT INTO ORDER_TABLE VALUES    
+    (16, 22, '2023-06-25', 'p016', 'furniture', '2023-06-26', '2023-06-30', 'Location_F', 'Location_K'),
+    (17, 23, '2023-05-18', 'p017', 'electronics', '2023-05-20', '2023-05-23', 'Location_B', 'Location_J'),
+    (18, 24, '2023-04-08', 'p018', 'cosmetics', '2023-04-10', '2023-04-15', 'Location_A', 'Location_D'),
+    (19, 25, '2023-03-15', 'p019', 'groceries', '2023-03-18', '2023-03-20', 'Location_E', 'Location_L'),
+    (20, 26, '2023-02-20', 'p020', 'clothing', '2023-02-22', '2023-02-25', 'Location_D', 'Location_A');
+    
+INSERT INTO ORDER_TABLE VALUES    
+    (21, 27,'2023-06-30', 'p021', 'electronics', '2023-07-01', '2023-07-05', 'Location_F', 'Location_H'),
+    (22, 28, '2023-05-25', 'p022', 'books', '2023-05-27', '2023-05-30', 'Location_B', 'Location_G'),
+    (23, 29, '2023-04-12', 'p023', 'toys', '2023-04-14', '2023-04-18', 'Location_A', 'Location_C'),
+    (24, 30, '2023-03-18', 'p024', 'clothing', '2023-03-20', '2023-03-23', 'Location_E', 'Location_D'),
+    (25, 31, '2023-02-25', 'p025', 'home decor', '2023-02-27', '2023-03-02', 'Location_D', 'Location_I');
+    
+    
+INSERT INTO ORDER_TABLE VALUES
+    (26, 32, '2023-07-05', 'p026', 'furniture', '2023-07-07', '2023-07-10', 'Location_F', 'Location_K'),
+    (27, 33, '2023-06-10', 'p027', 'electronics', '2023-06-12', '2023-06-15', 'Location_B', 'Location_J'),
+    (28, 34, '2023-05-15', 'p028', 'cosmetics', '2023-05-17', '2023-05-20', 'Location_A', 'Location_D'),
+    (29, 35, '2023-04-20', 'p029', 'groceries', '2023-04-22', '2023-04-25', 'Location_E', 'Location_L'),
+    (30, 36, '2023-03-25', 'p030', 'clothing', '2023-03-27', '2023-03-30', 'Location_D', 'Location_A');
+    
+INSERT INTO ORDER_TABLE VALUES
+    (31, 37, '2023-08-01', 'p031', 'electronics', '2023-08-02', '2023-08-06', 'Location_F', 'Location_H'),
+    (32, 38, '2023-07-05', 'p032', 'books', '2023-07-07', '2023-07-10', 'Location_B', 'Location_G'),
+    (33, 39, '2023-06-12', 'p033', 'toys', '2023-06-14', '2023-06-18', 'Location_A', 'Location_C'),
+    (34, 40, '2023-05-18', 'p034', 'clothing', '2023-05-20', '2023-05-23', 'Location_E', 'Location_D'),
+    (35, 41, '2023-04-25', 'p035', 'home decor', '2023-04-27', '2023-04-30', 'Location_D', 'Location_I');
+    
+INSERT INTO ORDER_TABLE VALUES
+    (36, 42, '2023-09-05', 'p036', 'furniture', '2023-09-07', '2023-09-10', 'Location_F', 'Location_K'),
+    (37, 43, '2023-08-10', 'p037', 'electronics', '2023-08-12', '2023-08-15', 'Location_B', 'Location_J'),
+    (38, 44, '2023-07-15', 'p038', 'cosmetics', '2023-07-17', '2023-07-20', 'Location_A', 'Location_D'),
+    (39, 45, '2023-06-20', 'p039', 'groceries', '2023-06-22', '2023-06-25', 'Location_E', 'Location_L'),
+    (40, 46, '2023-05-25', 'p040', 'clothing', '2023-05-27', '2023-05-30', 'Location_D', 'Location_A');
+
+INSERT INTO ORDER_TABLE VALUES
+    (41, 67, '2023-10-01', 'p041', 'electronics', '2023-10-02', '2023-10-06', 'Location_F', 'Location_H'),
+    (42, 68, '2023-09-05', 'p042', 'books', '2023-09-07', '2023-09-10', 'Location_B', 'Location_G'),
+    (43, 69, '2023-08-12', 'p043', 'toys', '2023-08-14', '2023-08-18', 'Location_A', 'Location_C'),
+    (44, 70, '2023-07-18', 'p044', 'clothing', '2023-07-20', '2023-07-23', 'Location_E', 'Location_D'),
+    (45, 71, '2023-06-25', 'p045', 'home decor', '2023-06-27', '2023-06-30', 'Location_D', 'Location_I');
+    
+    
+  INSERT INTO ORDER_TABLE VALUES  
+    (46, 72, '2023-11-05', 'p046', 'furniture', '2023-11-07', '2023-11-10', 'Location_F', 'Location_K'),
+    (47, 73, '2023-10-10', 'p047', 'electronics', '2023-10-12', '2023-10-15', 'Location_B', 'Location_J'),
+    (48, 74, '2023-09-15', 'p048', 'cosmetics', '2023-09-17', '2023-09-20', 'Location_A', 'Location_D'),
+    (49, 75, '2023-08-20', 'p049', 'groceries', '2023-08-22', '2023-08-25', 'Location_E', 'Location_L'),
+    (50, 76, '2023-07-25', 'p050', 'clothing', '2023-07-27', '2023-07-30', 'Location_D', 'Location_A');
+    
+INSERT INTO ORDER_TABLE VALUES
+    (51, 82, '2023-12-01', 'p051', 'electronics', '2023-12-02', '2023-12-06', 'Location_F', 'Location_H'),
+    (52, 83, '2023-11-05', 'p052', 'books', '2023-11-07', '2023-11-10', 'Location_B', 'Location_G'),
+    (53, 84, '2023-10-12', 'p053', 'toys', '2023-10-14', '2023-10-18', 'Location_A', 'Location_C'),
+    (54, 85, '2023-09-18', 'p054', 'clothing', '2023-09-20', '2023-09-23', 'Location_E', 'Location_D'),
+    (55, 86, '2023-08-25', 'p055', 'home decor', '2023-08-27', '2023-08-30', 'Location_D', 'Location_I');
+    
+INSERT INTO ORDER_TABLE VALUES
+    (56, 87, '2024-01-05', 'p056', 'furniture', '2024-01-07', '2024-01-10', 'Location_F', 'Location_K'),
+    (57, 88, '2023-12-10', 'p057', 'electronics', '2023-12-12', '2023-12-15', 'Location_B', 'Location_J'),
+    (58, 89, '2023-11-15', 'p058', 'cosmetics', '2023-11-17', '2023-11-20', 'Location_A', 'Location_D'),
+    (59, 90, '2023-10-20', 'p059', 'groceries', '2023-10-22', '2023-10-25', 'Location_E', 'Location_L'),
+    (60, 91, '2023-09-25', 'p060', 'clothing', '2023-09-27', '2023-09-30', 'Location_D', 'Location_A');
+
+SELECT * FROM ORDER_TABLE;
